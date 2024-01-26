89e6cb3de705c84a8214a0cc7940d51d6a563353
VaisakhPushpan <112495591+VaisakhPushpan@users.noreply.github.com>
2024-01-26 14:07:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vite-deploy-demo/',
   server : {
     host : 'localhost',
     port : 3000,
